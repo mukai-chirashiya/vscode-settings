aae9d1ddbbd958a7ebc4076b2e211b1d32a92a05
N.Mukai <mukai@chirashiya.co.jp>
2021-10-05 10:34:04 +0900

    add

--- test.js
+++ test.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded",()=>{
       document.getElementById("sample").addEventListener("click",e=>{
             console.log("hello")
       })
-      const hello = 'hello';
-      const hello2 = hello + 'hel'
-      console.log(hello)
+
+      
     })
